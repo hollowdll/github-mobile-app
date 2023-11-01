d766d7b227c66999dff1435b2947020c0cd64c65
hollowdll <pixerchat@gmail.com>
2023-11-01 14:51:08 +0200

    svg transformer

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,11 +1,13 @@
 import { View, StyleSheet } from 'react-native';
 import { Button, Text } from '@rneui/themed';
+import GitHubIcon from '../assets/mark-github.svg';
 
 // Login entry point
 // TODO: functionality
 export default function Login() {
   return (
     <View style={styles.loginContainer}>
+      <GitHubIcon style={{ marginBottom: 20 }} width={64} height={64} />
       <Text style={styles.infoText}>You need a GitHub account to use this app</Text>
       <Button
         title="Continue with GitHub"
