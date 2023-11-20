b6f70178bd762a14f29950b51b594b0cacdc2911
hollowdll <pixerchat@gmail.com>
2023-11-20 12:30:29 +0200

    repo test data and pagination

--- components/RepositoryList.tsx
+++ components/RepositoryList.tsx
@@ -6,23 +6,33 @@ import {
   VStack,
   Button,
   ButtonText,
-  ScrollView,
   ChevronLeftIcon,
   ChevronRightIcon,
   ButtonIcon,
 } from "@gluestack-ui/themed";
-import { FlatList } from "react-native";
-import { StyleSheet } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import { Repository } from "../types/repository";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { convertToLocaleDateString } from "../utility/date";
+
+const REPOS_PER_PAGE = 10;
+const DEFAULT_PAGE_NUMBER = 1;
 
 // Shows user repositories
 export default function RepositoryList() {
   const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [page, setPage] = useState(DEFAULT_PAGE_NUMBER);
+  const [isNextDisabled, setIsNextDisabled] = useState(true);
+  const scrollRef = useRef<ScrollView>(null);
 
-  // test data
-  useEffect(() => {
+  const scrollToTop = () => {
+    scrollRef.current?.scrollTo({
+      y: 0,
+      animated: true
+    });
+  }
+
+  const setPage1Repos = () => {
     setRepositories(
       [
         { id: 1, name: "Test repo 1", description: "Test Desc", createdAt: new Date(Date.now()), language: "Java" } as Repository,
@@ -37,12 +47,67 @@ export default function RepositoryList() {
         { id: 10, name: "Test repo 3", description: "Test Desc", createdAt: new Date(Date.now()), language: "C#" } as Repository,
       ]
     );
+  }
+
+  const setPage2Repos = () => {
+    setRepositories(
+      [
+        { id: 1, name: "Page 2", description: "Test Desc", createdAt: new Date(Date.now()), language: "Java" } as Repository,
+        { id: 2, name: "Page 2", description: "Test Desc", createdAt: new Date(Date.now()), language: "Rust" } as Repository,
+        { id: 3, name: "Page 2", description: "Test Desc", createdAt: new Date(Date.now()), language: "C#" } as Repository,
+        { id: 4, name: "Page 2", description: "Test Desc", createdAt: new Date(Date.now()), language: "C#" } as Repository,
+        { id: 5, name: "Page 2", description: "Test Desc", createdAt: new Date(Date.now()), language: "C#" } as Repository,
+        { id: 6, name: "Page 2", description: "Test Desc", createdAt: new Date(Date.now()), language: "C#" } as Repository,
+        { id: 7, name: "Test repo 3", description: "Test Desc", createdAt: new Date(Date.now()), language: "C#" } as Repository,
+        { id: 8, name: "Test repo 3", description: "Test Desc", createdAt: new Date(Date.now()), language: "C#" } as Repository,
+        { id: 9, name: "Test repo 3", description: "Test Desc", createdAt: new Date(Date.now()), language: "C#" } as Repository,
+        { id: 10, name: "Page 2", description: "Test Desc", createdAt: new Date(Date.now()), language: "C#" } as Repository,
+      ]
+    );
+  }
+
+  const setPage3Repos = () => {
+    setRepositories(
+      [
+        { id: 1, name: "Page 3 repo", description: "Test Desc", createdAt: new Date(Date.now()), language: "Java" } as Repository,
+        { id: 2, name: "testest", description: "Test Desc", createdAt: new Date(Date.now()), language: "Rust" } as Repository,
+        { id: 3, name: "yo", description: "Test Desc", createdAt: new Date(Date.now()), language: "C#" } as Repository,
+        { id: 4, name: "sup bro", description: "Test Desc", createdAt: new Date(Date.now()), language: "C#" } as Repository,
+      ]
+    );
+  }
+
+  // User goes to previous page
+  const handleOnPreviousPage = () => {
+    if (page === 2) setPage1Repos();
+    else if (page === 3) setPage2Repos();
+    else if (page === 4) setPage3Repos();
+
+    if (page <= DEFAULT_PAGE_NUMBER) setPage(DEFAULT_PAGE_NUMBER)
+    else setPage(page => page - 1);
+
+    scrollToTop();
+  }
+
+  // User goes to next page
+  const handleOnNextPage = () => {
+    if (page === 1) setPage2Repos()
+    else if (page === 2) setPage3Repos();
+
+    setPage(page => page + 1);
+
+    scrollToTop();
+  }
+
+  // test data
+  useEffect(() => {
+    setPage1Repos();
   }, []);
 
   return (
     <Box m="$3">
-      <ScrollView>
-        {repositories.map((item, index) => (
+      <ScrollView ref={scrollRef}>
+        {repositories.map((item, _index) => (
           <Box borderBottomWidth="$1" py="$2" borderColor="$trueGray800" key={item.id}>
             <HStack space="md" justifyContent="space-between">
               <VStack>
@@ -55,11 +120,12 @@ export default function RepositoryList() {
           </Box>
         ))}
         <HStack mt="$4" space="md" justifyContent="space-between">
-          <Button>
+          <Button isDisabled={page === DEFAULT_PAGE_NUMBER} onPress={handleOnPreviousPage}>
             <ButtonIcon as={ChevronLeftIcon} />
             <ButtonText> Previous</ButtonText>
           </Button>
-          <Button>
+          <Text mt="$2" style={styles.info}>Page { page }</Text>
+          <Button isDisabled={repositories.length !== REPOS_PER_PAGE} onPress={handleOnNextPage}>
             <ButtonText>Next </ButtonText>
             <ButtonIcon as={ChevronRightIcon} />
           </Button>
